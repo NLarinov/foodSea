b7e9ea4936f18b32496d059b20ae6e86c254ec5c
NLarinov <tkachev.n.1506@gmail.com>
2026-03-06 17:03:08 +0300

    feat: wire real services in DIContainer

--- foodSea/Core/DI/DIContainer.swift
+++ foodSea/Core/DI/DIContainer.swift
@@ -2,43 +2,42 @@ import Foundation
 
 final class DIContainer: @unchecked Sendable {
     let useMocks: Bool
+    let authService: any AuthServiceProtocol
+    let productService: any ProductServiceProtocol
+    let cartService: any CartServiceProtocol
+    let orderService: any OrderServiceProtocol
+    let optimizationService: any OptimizationServiceProtocol
+    let voiceService: any VoiceServiceProtocol
 
     init(useMocks: Bool = true) {
         self.useMocks = useMocks
+        if useMocks {
+            authService = MockAuthService()
+            productService = MockProductService()
+            cartService = MockCartService()
+            orderService = MockOrderService()
+            optimizationService = MockOptimizationService()
+            voiceService = MockVoiceService()
+        } else {
+            let tokenStore = AuthTokenStore()
+            let coreURL = Constants.API.coreBaseURL
+            let coreClient = NetworkClient(baseURL: coreURL, tokenStore: tokenStore)
+            let optClient = NetworkClient(
+                baseURL: Constants.API.optimizationBaseURL,
+                refreshBaseURL: coreURL,
+                tokenStore: tokenStore
+            )
+            let ordClient = NetworkClient(
+                baseURL: Constants.API.orderingBaseURL,
+                refreshBaseURL: coreURL,
+                tokenStore: tokenStore
+            )
+            authService = RealAuthService(client: coreClient, tokenStore: tokenStore)
+            productService = RealProductService(coreClient: coreClient, optClient: optClient)
+            cartService = RealCartService(client: coreClient)
+            optimizationService = RealOptimizationService(client: optClient)
+            orderService = RealOrderService(client: ordClient)
+            voiceService = MockVoiceService()
+        }
     }
-
-    lazy var productService: any ProductServiceProtocol = {
-        if useMocks {
-            return MockProductService()
-        }
-        fatalError("Real ProductService not implemented")
-    }()
-
-    lazy var cartService: any CartServiceProtocol = {
-        if useMocks {
-            return MockCartService()
-        }
-        fatalError("Real CartService not implemented")
-    }()
-
-    lazy var orderService: any OrderServiceProtocol = {
-        if useMocks {
-            return MockOrderService()
-        }
-        fatalError("Real OrderService not implemented")
-    }()
-
-    lazy var optimizationService: any OptimizationServiceProtocol = {
-        if useMocks {
-            return MockOptimizationService()
-        }
-        fatalError("Real OptimizationService not implemented")
-    }()
-
-    lazy var voiceService: any VoiceServiceProtocol = {
-        if useMocks {
-            return MockVoiceService()
-        }
-        fatalError("Real VoiceService not implemented")
-    }()
 }
